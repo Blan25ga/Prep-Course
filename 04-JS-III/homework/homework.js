cb6b30dbfeee896711f4ffa5c3ce0c49f8e53bb8
Blan25co <gabriel.bl@hotmail.coma.ar>
2022-02-21 09:37:46 -0300

    17 del 04js

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -143,7 +143,7 @@ function cuentoElementos(arreglo){
   var contador= 0
   for(var i=0; i< arreglo.length; i++){
     if(arreglo[i] > 18){
-    contador = cintador +1
+    contador = contador +1
     }
   } return contador;
 
@@ -166,8 +166,13 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
-  
+  var str = n.toString();
+  if (str[0] === "9"){
+    return true;
+  } else {
+    return false;
+  }
+
 }
 
 
@@ -175,7 +180,12 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+  for (var i = 1; i < arreglo.length; i++){
+    if (arreglo[0] !== arreglo[i]){
+      return false
+    }
+  }
+  return true;
 } 
 
 
@@ -184,6 +194,23 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  var nuevoArray = [];
+
+  for (var i = 0; i < array.length; i++) {
+    if (
+      array[i] === 'Enero' ||
+      array[i] === 'Marzo'|| 
+      array[i] === 'Noviembre'
+    ){
+       nuevoArray.push(array[i]);
+    }
+  }
+  
+  if (nuevoArray.length < 3){
+    return 'No se encontraron los meses pedidos';
+  } else {
+    return nuevoArray;
+  }
 }
 
 
@@ -191,6 +218,12 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  var nuevoArray = [];
+
+  for (var i = 0; i < array.length; i++) {
+    if (array [i] > 100) array.push (array [i]);
+  } 
+  return nuevoArray;
 }
 
 

7f1532e7845b30a59cbbcacb8626f4a90035e12e
Blan25co <gabriel.bl@hotmail.coma.ar>
2022-02-21 11:12:28 -0300

    finalizacion de jsIII

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -220,8 +220,9 @@ function mayorACien(array) {
   // Tu código:
   var nuevoArray = [];
 
-  for (var i = 0; i < array.length; i++) {
-    if (array [i] > 100) array.push (array [i]);
+  for (var i = 0; i < array.length; i++){
+    if (array [i] > 100) {
+      nuevoArray.push (array [i])};
   } 
   return nuevoArray;
 }
@@ -235,6 +236,25 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var array = [];
+
+  var suma = numero;
+
+  for ( var i = 0; i < 10; i++) {
+    suma = suma + 2 
+    if (suma === i){
+      break;
+    } else {
+      array.push(suma);
+    }
+  }
+
+  if (i < 10) {
+    return "Se interrumpió la ejecución"
+  } else {
+    return array; 
+  }
+
 }
 
 
@@ -245,6 +265,19 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var array = [];
+  var suma = numero;
+
+  for (var i = 0; i < 10; i++) {
+    if (i === 5) {
+      continue;
+    } else {
+      suma = suma + 2; 
+      array.push(suma);
+    }
+  }
+  return array;
+
 }
 
 
